4288ea1fa234c924099753b7907354a98772c266
Szilard7593 <barthaszilard@icloud.com>
2025-11-22 15:13:12 +0200

    operatii pe siruri de dublu cuvinte

---
+++ Lab3_Sectiunea2.asm
@@ -0,0 +1,37 @@
+assume cs: code, ds:data
+data SEGMENT
+a dd 0000110Ah, 10100000Dh, 00000003h, 20000004h
+l equ ($ - a)/4
+d db l dup (? )
+data ENDS
+code SEGMENT
+START:
+mov ax, data
+mov ds, ax
+mov es, ax
+cld ;DF = 0
+mov si, offset a;0
+mov cx, l;2
+mov di, offset d;0
+jcxz final
+
+repeta:
+    lodsw                  ;citim cuvantul inferior
+    lodsw                  ;citim cuvantul superior
+                           ;punem word-urile in acumulator ca sa putem face operatiile pe ele care le dorim
+    test al, 00000001b     ;verificam daca e par sau impar   
+    jnz skip_par_a         ;daca CF = 1 repetam din nou  
+    
+    test al, 10000000b     ;verificam daca e negativ, uitandune la bitul de semn 
+    jz skip_par_a          ;daca e zero, repetam din nou loop-ul   
+    stosb                  ;acum putem sa-l stocam in destinatie
+    
+skip_par_a:
+    loop repeta   
+            
+
+final:
+mov ax, 4C00H
+int 21h
+code ENDS
+end START
