bba431e5745f5591da123f5eb2c5e342ed9479ab
Szilard7593 <barthaszilard@icloud.com>
2025-11-25 18:24:26 +0200

    Add files via upload

--- Lab3.asm
+++ Lab3.asm
@@ -15,44 +15,46 @@ START:
     mov ds, ax
     mov es, ax
 
-    mov si , offset b 
-    mov cx, l_b ; punem in cx lungimea lui b
-    add si, l_b ; punem soarce indexul la   finalul lui b
-    dec si ; si ponteaz actre ultima pozitie a lui b , 
-            ;pentru ca daca lansam pozitia normala el va fi pointer la un element ca nu exista
+    mov si , offset b   ; punem in cx lungimea lui b
+    add si, l_b         ; punem soarce indexul la finalul lui b
+    dec si              ; si ponteaz actre ultima pozitie a lui b , 
+                        ;pentru ca daca lansam pozitia normala el va fi pointer la un element ca nu exista
     mov di, offset r
- ; di pointeaz actre ultima pozitie a lui r
-    ; setam DF = 1 pentru a copia de la sfarsit catre inceput
 
-                                         ;sau oare mergea mov al , [si]
-                                            ;mov [di] ,al
-                                            ;dec si?care ii mai eficient?
-    loop_b:                                    ;inc di
+                        ; di pointeaz actre ultima pozitie a lui r
+                        ; setam DF = 1 pentru a copia de la sfarsit catre inceput
+    mov cx, l_b
+
+                        ;sau oare mergea mov al , [si]
+                        ;mov [di] ,al
+                        ;dec si?care ii mai eficient?
+                        ;inc di
+    cld
+    loop_b:                                     
         std
         lodsb
         cld
         stosb
         loop loop_b
-    cld  
-        ; la final setam DF = 0
 
     mov si, offset a
-    mov cx, l_a      ; punem in cx lungimea lui a
-    loop_a:     ; verificam care elemente din a sunt impare, folosind test, care creeaza o operatie de and intre al si o masca
-        lodsb; luam urmatorul element din a
-        test al,1;verificam daca e par sau impar, echiva;ent cu puteam sa 
-        ;mov bl, al
-        ;and bl, 1
-        ;mov al,bl
-        ;jnz skip_par_a
-        ;stosb
+    mov cx, l_a         ; punem in cx lungimea lui a
+    cld                 ; la final setam DF = 0
+    loop_a:             ; verificam care elemente din a sunt impare, folosind test, care creeaza o operatie de and intre al si o masca
+        lodsb           ; luam urmatorul element din a
+        test al,1       ;verificam daca e par sau impar, echiva;ent cu puteam sa 
+                        ;mov bl, al
+                        ;and bl, 1
+                        ;mov al,bl
+                        ;jz skip_par_a
+                        ;stosb
 
         jz skip_par_a
-        stosb; punem elementul impar in r
+        stosb           ;punem elementul impar in r
 
     skip_par_a:
-        loop loop_a ; facem din nou loop-ul
-
+        loop loop_a ;facem din nou loop-ul
+    
     mov ax, 4C00H
     int 21h
 code ENDS
